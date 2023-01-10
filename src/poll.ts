2edaa807ef5e4e9e7e7c8a1031add654c276a706
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2023-01-10 13:08:07 +0100

    Format

--- src/poll.ts
+++ src/poll.ts
@@ -18,17 +18,7 @@ export interface Options {
 }
 
 export const poll = async (options: Options): Promise<string> => {
-  const {
-    client,
-    log,
-    checkName,
-    timeoutSeconds,
-    intervalSeconds,
-    warmupSeconds,
-    owner,
-    repo,
-    ref
-  } = options
+  const {client, log, checkName, timeoutSeconds, intervalSeconds, warmupSeconds, owner, repo, ref} = options
 
   let now = new Date().getTime()
   const deadline = now + timeoutSeconds * 1000
@@ -36,9 +26,7 @@ export const poll = async (options: Options): Promise<string> => {
   let foundRun = false
 
   while (now <= deadline) {
-    log(
-      `Retrieving check runs named ${checkName} on ${owner}/${repo}@${ref}...`
-    )
+    log(`Retrieving check runs named ${checkName} on ${owner}/${repo}@${ref}...`)
     const result = await client.rest.checks.listForRef({
       check_name: checkName,
       owner,
@@ -46,43 +34,30 @@ export const poll = async (options: Options): Promise<string> => {
       ref
     })
 
-    log(
-      `Retrieved ${result.data.check_runs.length} check runs named ${checkName}`
-    )
+    log(`Retrieved ${result.data.check_runs.length} check runs named ${checkName}`)
 
     foundRun = foundRun || result.data.check_runs.length !== 0
 
     if (now >= warmupDeadline && !foundRun) {
-      log(
-        `No checks found after ${warmupSeconds} seconds, exiting with conclusion 'not_found'`
-      )
+      log(`No checks found after ${warmupSeconds} seconds, exiting with conclusion 'not_found'`)
       return 'not_found'
     }
 
     const lastStartedCheck = getLastStartedCheck(result.data.check_runs)
-    if (
-      lastStartedCheck !== undefined &&
-      lastStartedCheck.status === 'completed'
-    ) {
-      log(
-        `Found a completed check with id ${lastStartedCheck.id} and conclusion ${lastStartedCheck.conclusion}`
-      )
+    if (lastStartedCheck !== undefined && lastStartedCheck.status === 'completed') {
+      log(`Found a completed check with id ${lastStartedCheck.id} and conclusion ${lastStartedCheck.conclusion}`)
       // conclusion is only `null` if status is not `completed`.
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       return lastStartedCheck.conclusion!
     }
 
-    log(
-      `No completed checks named ${checkName}, waiting for ${intervalSeconds} seconds...`
-    )
+    log(`No completed checks named ${checkName}, waiting for ${intervalSeconds} seconds...`)
     await wait(intervalSeconds * 1000)
 
     now = new Date().getTime()
   }
 
-  log(
-    `No completed checks after ${timeoutSeconds} seconds, exiting with conclusion 'timed_out'`
-  )
+  log(`No completed checks after ${timeoutSeconds} seconds, exiting with conclusion 'timed_out'`)
   return 'timed_out'
 }
 function getLastStartedCheck(checks: CheckRun[]): CheckRun | undefined {
