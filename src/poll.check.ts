25e1179fe6408bf5018d4348ee3cffaae5c19952
Jeroen Bos <jer123bos@gmail.com>
2023-02-06 15:49:28 +0100

    Move to other PR

--- src/poll.check.ts
+++ src/poll.check.ts
@@ -19,7 +19,7 @@ class CheckPoller implements Poller<Options> {
 
     const lastStartedCheck = this.getLastStartedCheck(result.data.check_runs)
     if (lastStartedCheck !== undefined && lastStartedCheck.status === 'completed') {
-      log(`Found a completed check with id ${lastStartedCheck.id} and conclusion '${lastStartedCheck.conclusion}'`)
+      log(`Found a completed check with id ${lastStartedCheck.id} and conclusion ${lastStartedCheck.conclusion}`)
       // conclusion is only `null` if status is not `completed`.
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       return lastStartedCheck.conclusion!
